84f4142aca3a05716a33a2344aa08773b87a023b
Fedortsov Dmytro <202955090+FedortsovDmytro@users.noreply.github.com>
2026-01-26 13:42:15 +0100

    read data from exel

---
+++ src/main/java/com/example/demo/ekselUploud/controller/ClientImportController.java
@@ -0,0 +1,4 @@
+package com.example.demo.ekselUploud.controller;
+
+public class ClientImportController {
+}
--- src/main/java/com/example/demo/ekselUploud/servise/BisnodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.ekselUploud.servise;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BisnodeRepository extends JpaRepository<Bisnode, Long> {
-    Optional<Bisnode> findByExternalId(Long externalId);
-}
