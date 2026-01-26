371dda5ea88e361add8422cd734fdfab86e99b33
Fedortsov Dmytro <202955090+FedortsovDmytro@users.noreply.github.com>
2026-01-26 20:38:06 +0100

    adding controller for third(raport_pod_hermesa)

---
+++ src/main/java/com/example/demo/ekselUploud/reposytory/ReceivableAgingRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.ekselUploud.reposytory;
+
+public interface ReceivableAgingRepository {
+}
---
+++ src/main/java/com/example/demo/ekselUploud/servise/ReceivableAgingExcelService.java
@@ -0,0 +1,4 @@
+package com.example.demo.ekselUploud.servise;
+
+public class ReceivableAgingExcelService {
+}
